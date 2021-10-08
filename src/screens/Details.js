abd7383c0022d968dbb69830ef9736968f4bafa9
didarulamin <programmer.didarul@gmail.com>
2021-10-08 17:09:48 +0600

    details screen added

--- src/screens/Details.js
+++ src/screens/Details.js
@@ -1,14 +1,141 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import {
+  Image,
+  StyleSheet,
+  View,
+  ScrollView,
+  Linking,
+  Pressable,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "../components/Header/Header";
 import Text from "../components/text/text";
+import { spacing } from "./../theme/spacing";
 
-const Details = () => {
+const Details = ({ route }) => {
+  const { planet } = route.params;
+  const {
+    name,
+    surfaceImage,
+    description,
+    rotationTime,
+    revolutionTime,
+    avgTemp,
+    radius,
+    wikiLink,
+  } = planet;
+
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.container}>
       <Header></Header>
-    </SafeAreaView>
+      <ScrollView>
+        <Image source={surfaceImage} style={{ alignSelf: "center" }}></Image>
+        <Text
+          preset="h1"
+          style={{
+            textAlign: "center",
+            marginTop: spacing[4],
+            textTransform: "uppercase",
+          }}
+        >
+          {name}
+        </Text>
+        <Text
+          style={{ textAlign: "center", marginTop: spacing[4], fontSize: 18 }}
+        >
+          {description}
+        </Text>
+
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            marginTop: spacing[2],
+          }}
+        >
+          <Text>Source: </Text>
+          <Pressable onPress={() => Linking.openURL(wikiLink)}>
+            <Text style={{ textDecorationLine: "underline" }}>Wikipedia</Text>
+          </Pressable>
+        </View>
+
+        <View style={{ justifyContent: "center", margin: 10 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              padding: 10,
+              borderWidth: 1,
+              borderColor: "grey",
+              margin: 10,
+              padding: 15,
+            }}
+          >
+            <Text style={{ textTransform: "uppercase", color: "grey" }}>
+              Rotation Time
+            </Text>
+            <Text preset="h2" style={{ textTransform: "uppercase" }}>
+              {rotationTime}
+            </Text>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              borderWidth: 1,
+              borderColor: "grey",
+              margin: 10,
+              padding: 15,
+            }}
+          >
+            <Text style={{ textTransform: "uppercase", color: "grey" }}>
+              Revolution Time
+            </Text>
+            <Text preset="h2" style={{ textTransform: "uppercase" }}>
+              {revolutionTime}
+            </Text>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              borderWidth: 1,
+              borderColor: "grey",
+              margin: 10,
+              padding: 15,
+            }}
+          >
+            <Text style={{ textTransform: "uppercase", color: "grey" }}>
+              Radius
+            </Text>
+            <Text preset="h2" style={{ textTransform: "uppercase" }}>
+              {radius}
+            </Text>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              borderWidth: 1,
+              borderColor: "grey",
+              margin: 10,
+              padding: 15,
+            }}
+          >
+            <Text style={{ textTransform: "uppercase", color: "grey" }}>
+              Average Time
+            </Text>
+            <Text preset="h2" style={{ textTransform: "uppercase" }}>
+              {avgTemp}
+            </Text>
+          </View>
+        </View>
+      </ScrollView>
+    </View>
   );
 };
 
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,17 +1,72 @@
 import React from "react";
-import { Button, Pressable, StyleSheet, View } from "react-native";
+import { Button, Pressable, StyleSheet, View, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "../components/Header/Header";
 import Text from "../components/text/text";
+import { PLANET_LIST } from "./../data/planetData";
+import { spacing } from "./../theme/spacing";
+import { AntDesign } from "@expo/vector-icons";
 
+console.log(PLANET_LIST);
 const Home = ({ navigation }) => {
+  const renderItem = ({ item, index }) => {
+    return (
+      <View>
+        <Pressable
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            padding: spacing[1],
+            justifyContent: "space-between",
+          }}
+          onPress={() => navigation.navigate("Details", { planet: item })}
+        >
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <View
+              style={{
+                width: 20,
+                height: 20,
+                borderRadius: 10,
+                backgroundColor: item.color,
+                margin: spacing[2],
+              }}
+            />
+            <Text
+              style={{
+                textTransform: "uppercase",
+                fontWeight: "bold",
+                fontSize: 20,
+              }}
+            >
+              {item.name}
+            </Text>
+          </View>
+          <AntDesign name="right" size={24} color="white" />
+        </Pressable>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
       <Header></Header>
 
-      <Pressable onPress={() => navigation.navigate("Details")}>
-        <Text>Go to Details</Text>
-      </Pressable>
+      <View>
+        <FlatList
+          data={PLANET_LIST}
+          renderItem={renderItem}
+          keyExtractor={(item, index) => item.name}
+          contentContainerStyle={{ padding: spacing[5] }}
+          ItemSeparatorComponent={() => (
+            <View
+              style={{
+                borderBottomWidth: 1,
+                borderBottomColor: "#979797",
+              }}
+            />
+          )}
+        />
+      </View>
     </View>
   );
 };
